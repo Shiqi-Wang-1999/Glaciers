352e108f4ddf11ae8cee1af7f8d14c5f6713c6ba
Shiqi Wang <shiqi_w@outlook.com>
2021-11-20 14:45:20 +0000

    Complete the 'filter_by_code' method and then test by 'User Interface' class

--- User Interface.py
+++ User Interface.py
@@ -3,8 +3,10 @@ from glaciers import Glacier, GlacierCollection
 
 file_path = Path("C:/PycharmProjects/data/sheet-A.csv")
 collection = GlacierCollection(file_path)
+# print(collection.Raw_Glacier_Collections)
+print(collection.filter_by_code("6?9"))
 
-file_path1 = Path("C:/PycharmProjects/data/sheet-EE.csv")
-collection.read_mass_balance_data(file_path1)
-for glacier in collection.Glacier_Collections:
-    print(glacier.mass_balance_measurement)
+# file_path1 = Path("C:/PycharmProjects/data/sheet-EE.csv")
+# collection.read_mass_balance_data(file_path1)
+# for glacier in collection.Glacier_Collections:
+#     print(glacier.mass_balance_measurement)
--- glaciers.py
+++ glaciers.py
@@ -1,5 +1,6 @@
 import codecs
 import csv
+import re
 
 class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
@@ -22,7 +23,7 @@ class Glacier:
     def plot_mass_balance(self, output_path):
         raise NotImplementedError
 
-        
+
 class GlacierCollection:
 
     def __init__(self, file_path):
@@ -36,11 +37,9 @@ class GlacierCollection:
                 for row in csv_reader:
                     self.Raw_Glacier_Collections.append(dict(row))
 
-
-
     def read_mass_balance_data(self, file_path):
         for item in self.Raw_Glacier_Collections:
-            three_digit = str(item['PRIM_CLASSIFIC']+item['FORM']+item['FRONTAL_CHARS'])
+            three_digit = str(item['PRIM_CLASSIFIC'] + item['FORM'] + item['FRONTAL_CHARS'])
             glacier = Glacier(item['WGMS_ID'], item['NAME'], item['POLITICAL_UNIT'],
                               float(item['LATITUDE']), float(item['LONGITUDE']),
                               int(three_digit))
@@ -62,16 +61,48 @@ class GlacierCollection:
                             pass
             self.Glacier_Collections.append(glacier)
 
-
-
-
     def find_nearest(self, lat, lon, n):
         """Get the n glaciers closest to the given coordinates."""
         raise NotImplementedError
-    
+
     def filter_by_code(self, code_pattern):
         """Return the names of glaciers whose codes match the given pattern."""
-        raise NotImplementedError
+        glacier_names = []
+        code_ = str(code_pattern)
+        ques_mark_num = code_pattern.count('?')
+        if ques_mark_num == 0:
+            for i in self.Raw_Glacier_Collections:
+                three_digit = i['PRIM_CLASSIFIC'] + i['FORM'] + i['FRONTAL_CHARS']
+                if three_digit == code_:
+                    glacier_names.append(i['NAME'])
+        elif ques_mark_num == 1:
+            index = int(code_pattern.find('?'))
+            iter = re.finditer("\d", code_pattern)
+            lis = []
+            for i in iter:
+                lis.append(code_pattern[i.span()[0]])
+            for i in self.Raw_Glacier_Collections:
+                if index == 0 & (i['FORM'] == lis[0]) & (i['FRONTAL_CHARS'] == lis[1]):
+                    glacier_names.append(i['NAME'])
+                elif index == 1 & (i['PRIM_CLASSIFIC'] == lis[0]) & (i['FRONTAL_CHARS'] == lis[1]):
+                    glacier_names.append(i['NAME'])
+                elif index == 2 & (i['PRIM_CLASSIFIC'] == lis[0]) & (i['FORM'] == lis[1]):
+                    glacier_names.append(i['NAME'])
+        elif ques_mark_num == 2:
+            index = int(re.search("\d",code_pattern).start())
+            value = re.search("\d", code_pattern).group()
+            for i in self.Raw_Glacier_Collections:
+                if (index == 0) & (i['PRIM_CLASSIFIC'] == value):
+                    glacier_names.append(i['NAME'])
+                elif (index == 1) & (i['FORM'] == value):
+                    glacier_names.append(i['NAME'])
+                elif (index == 2) & (i['FRONTAL_CHARS'] == value):
+                    glacier_names.append(i['NAME'])
+        else:
+            for i in self.Raw_Glacier_Collections:
+                glacier_names.append(i['NAME'])
+
+        return glacier_names
 
     def sort_by_latest_mass_balance(self, n, reverse):
         """Return the N glaciers with the highest area accumulated in the last measurement."""
